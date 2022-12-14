9d55b75ed14c4ecadf0a1fd4b821c47515411c01
Jamin Smith <112420938+JaminSmith@users.noreply.github.com>
2022-12-14 21:51:30 +0000

    yikers

--- script/database.js
+++ script/database.js
@@ -1,18 +1,13 @@
-var mysql = import('mysql');
+var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host:'10.10.21.98',
-    user: 'second-year-pm',
-    password: '@LongLivePetta',
-    database: 'student-tracker-pm.database.windows.net',
-    port: '3001'
+  host: "*",
+  user: "second-year-pm",
+  password: "@LongLivePetta",
+  port: '1433'
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
-
-app.listen(3001, () => {
-    console.log ('running on port 3001 yes')
-}) 
