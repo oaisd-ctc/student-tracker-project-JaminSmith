1fff23ebbb1cd3b53012e85f7c0356b912aa42af
Jamin Smith <112420938+JaminSmith@users.noreply.github.com>
2023-01-13 18:58:19 +0000

    yikerspasdf

--- script/database.js
+++ script/database.js
@@ -1,4 +1,4 @@
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://student-tracker-web-api-1.azurewebsites.net/Student/StudentInfo',)
   .then(response => response.json())
   .then(data => {
     let table = `<table>
@@ -6,9 +6,8 @@ fetch('https://jsonplaceholder.typicode.com/users')
                     <th>ID</th>
                     <th>FirstName</th>
                     <th>Last Name</th>
-                    <th>Class</th>
-                    <th>Phone</th>
-                    <th>Date</th>
+                    <th>Address</th>
+                    <th>Sending School</th>
                     <th>Time In</th>
                     <th>Time Out</th>
                     <th>Number of punchouts</th>
@@ -17,19 +16,17 @@ fetch('https://jsonplaceholder.typicode.com/users')
     data.forEach(item => {
       
       table += `<tr class="g">
-                  <td>${item.notInSampleData}</td>
-                  <td>${item.name}</td>
-                  <td>${item.name}</td>
-                  <td>${item.notInSampleData}</td>
-                  <td>${item.phone}</td>
-                  <td>${item.notInSampleData}</td>
-                  <td>${item.notInSampleData}</td>
-                  <td>${item.notInSampleData}</td>
+                  <td>${item.studentId}</td>
+                  <td>${item.firstName}</td>
+                  <td>${item.lastName}</td>
+                  <td>${item.addressLine1}</td>
+                  <td>${item.SendingSchool}</td>
+                  <td>${item.timeOut}</td>
+                  <td>${item.timeIn}</td>
                   <td>${item.notInSampleData}</td>
                   <td><button>View</button></td>
                 </tr>`;
     });
     table += '</table>';
     document.querySelector('#table-container').innerHTML = table;
-    
   });
