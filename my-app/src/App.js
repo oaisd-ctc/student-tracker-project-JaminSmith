5cab786e80d90e2bce640fd89269f36478f42b9d
Luke-Mika-STUDENT <112420925+Luke-Mika-STUDENT@users.noreply.github.com>
2023-01-23 15:48:53 +0000

    fixed some bugs now all the nav works (no refresh)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,23 +7,23 @@ import {Route, Routes} from "react-router-dom"
 
 function App() {
 
+  return (
+    <>
+    <Navbar/>
+    <div className="container">
+    <Routes>
+      <Route path="/" element= {<Home/>}/>
+      <Route path="/Feedback" element= {<Feedback/>}/>
+      <Route path="/Tracker" element= {<Tracker/>}/>
+      <Route path="/TrackerEdit" element= {<TrackerEdit/>}/>
+    </Routes>
+    </div>
+    
+    </>
+    )
+
 
       
   }
-  return (
-  <>
-  <Navbar/>
-  <div className="container">
-  <Routes>
-    <Route path="/" element= {<Home/>}/>
-    <Route path="/Feedback" element= {<Feedback/>}/>
-    <Route path="/Tracker" element= {<Tracker/>}/>
-    <Route path="/TrackerEdit" element= {<TrackerEdit/>}/>
-  </Routes>
-  </div>
-  
-  </>
-  )
-}
 
 export default App;
--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom"
+import {Link, resolvePath, useMatch, useResolvedPath} from "react-router-dom"
 export default function Navbar() {
 
     return <nav className="nav"> 
@@ -12,9 +12,10 @@ export default function Navbar() {
 }
 
 function CustomLink({ to, children, ...props}) {
-    const path = window.location.pathname
+    const resolvedPath =useResolvedPath(to)
+    const isActive= useMatch({path:resolvedPath.pathname, end:true})
     return (
-        <li className={path === to ? "active" : ""}>
+        <li className={isActive ? "active" : ""}>
             <Link to={to} {...props}>{children}</Link>
         </li>
     )
