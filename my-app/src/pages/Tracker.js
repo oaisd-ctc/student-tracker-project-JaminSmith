086ce8e9c43314ff270e4a80613536a507a9f096
Luke-Mika-STUDENT <112420925+Luke-Mika-STUDENT@users.noreply.github.com>
2023-01-23 17:44:32 +0000

    fdjld; goihfd

--- my-app/src/pages/Tracker.js
+++ my-app/src/pages/Tracker.js
@@ -1,8 +1,52 @@
-export default function Tracker() {
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+require('./tracker.css')
+const Table = () => {
+  const [students, setStudents] = useState([]);
+  useEffect(() => {
+    const getStudents = async () => {
+      try {
+        const response = await axios.get('https://student-tracker-web-api-1.azurewebsites.net/api/controller/StudentInfo', {
+          headers: {
+            'ApiKey':'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'
+          }
+        });
+        setStudents(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getStudents();
+  }, []);
   return (
-      <h1>Welcome to the Tracker page</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Student ID</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Address Line 1</th>
+          <th>Sending School</th>
+          <th>Time Out</th>
+          <th>Time In</th>
+          <th>Object1</th>
+        </tr>
+      </thead>
+      <tbody>
+        {students.map((student) => (
+          <tr key={student.studentID}>
+            <td>{student.studentID}</td>
+            <td>{student.firstName}</td>
+            <td>{student.lastName}</td>
+            <td>{student.addressLine1}</td>
+            <td>{student.sendingSchool}</td>
+            <td>{student.timeOut}</td>
+            <td>{student.timeIn}</td>
+            <td>{student.object1}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
-  }
-
-  
+}
+export default Table;
